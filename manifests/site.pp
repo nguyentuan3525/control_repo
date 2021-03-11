57178fad8e0688540e6096a599270921bcb6a791
nguyentuan3525 <48991553+nguyentuan3525@users.noreply.github.com>
2021-03-11 14:32:10 +0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,10 +3,6 @@ node default {
     ensure => file,
     content => 'This a me new',
     }
-    file {'/tmp/tuan1':
-    ensure => file,
-    content => 'This is a tmp',
-    }
 }
 node 'master.puppet.vm' {
     include role::master_server
